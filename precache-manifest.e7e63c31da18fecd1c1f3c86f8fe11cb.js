905fe737e2b214f017efc7facbf670029516e000
Pedro del Rio <pedelriomarron@gmail.com>
2020-03-18 03:18:00 +0100

    Updates

---
+++ precache-manifest.e7e63c31da18fecd1c1f3c86f8fe11cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c635a5e90e342982d3ec4bb60660bc4",
+    "url": "/examples-redux-hooks_react/index.html"
+  },
+  {
+    "revision": "5ccadcde3607ec9b38ec",
+    "url": "/examples-redux-hooks_react/static/css/main.87f0540f.chunk.css"
+  },
+  {
+    "revision": "90b555ab12dc81f4d15d",
+    "url": "/examples-redux-hooks_react/static/js/2.c8e580ed.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/examples-redux-hooks_react/static/js/2.c8e580ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccadcde3607ec9b38ec",
+    "url": "/examples-redux-hooks_react/static/js/main.88a46309.chunk.js"
+  },
+  {
+    "revision": "1718272ef715c0352f80",
+    "url": "/examples-redux-hooks_react/static/js/runtime-main.e51cf55f.js"
+  }
+]);
